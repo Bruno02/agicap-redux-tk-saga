217bd742c9b10b7bbb8aebe6f0749d4893063e2f
Bruno Fima <bruno.fima@aktome.fr>
2021-07-23 11:08:45 +0200

    List test with const

--- app/components/list/List.test.tsx
+++ app/components/list/List.test.tsx
@@ -6,12 +6,19 @@ import List from './List';
 
 describe('<List />', () => {
   it('List has a given element and get elements count', () => {
-    const items = ['one', 'two', 'three', 'four', 'five'];
+    const ITEMS = ['one', 'two', 'three', 'four', 'five'];
 
     const FLAT_LIST_TEST_ID = 'flatListTest';
 
-    const { getByText, getByTestId } = render(<List items={items} testID={FLAT_LIST_TEST_ID} />);
-    expect(getByText(items[4]));
-    expect(getByTestId(FLAT_LIST_TEST_ID).props.data.length).toEqual(items.length);
+    const { getByText, getByTestId } = render(<List items={ITEMS} testID={FLAT_LIST_TEST_ID} />);
+    expect(getByText('two'));
+    expect(getByTestId(FLAT_LIST_TEST_ID).props.data.length).toEqual(5);
+    expect(getByTestId(FLAT_LIST_TEST_ID).props.data).toEqual([
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+    ]);
   });
 });
